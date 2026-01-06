b48e59940c720d543576d8e82781e3380980afcd
Luis Felipe <luisrochacruzalves@gmail.com>
2026-01-06 19:13:54 -0300

    feat(docs): enhance Tailwind CSS integration and update configuration
    - Add Tailwind CSS and PostCSS dependencies to the documentation app for improved styling capabilities.
    - Update the Next.js configuration to streamline the import paths for MDX components.
    - Refactor global CSS to utilize new Tailwind features and simplify the structure.
    - Introduce a new PostCSS configuration file to manage Tailwind CSS plugins effectively.

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,30 +1,13 @@
 import nextra from "nextra";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const withNextra = nextra({
-  contentDirBasePath: "/content",
-});
+const withNextra = nextra({});
 
 export default withNextra({
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "next-mdx-import-source-file": path.resolve(
-        __dirname,
-        "./src/components/mdx-components.tsx"
-      ),
-    };
-    return config;
-  },
   experimental: {
     turbo: {
       resolveAlias: {
-        "next-mdx-import-source-file": "./src/components/mdx-components.tsx",
+        "next-mdx-import-source-file": "./src/mdx-components.tsx",
       },
     },
   },
